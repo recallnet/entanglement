d1cc2a99cc93d04ca120beea766c240e6d19a43b
Islam Aliev <aliev.islam@gmail.com>
2024-09-09 15:16:10 +0200

    Fix creation of iroh storage from node and client.

--- storage/src/iroh.rs
+++ storage/src/iroh.rs
@@ -5,22 +5,32 @@ use anyhow::Result;
 use async_trait::async_trait;
 use bytes::Bytes;
 use core::net::SocketAddr;
-use futures::stream::{self, Stream};
+use futures::stream;
 use iroh::{blobs::Hash, client::Iroh as Client};
-use std::{path::Path, pin::Pin, str::FromStr};
+use std::sync::Arc;
+use std::{path::Path, str::FromStr};
 
-use crate::storage::Storage;
+use crate::storage::{ByteStream, Error as StorageError, Storage};
 
 const CHUNK_SIZE: usize = 1024;
-
-pub struct IrohStorage {
-    client_provider: Box<dyn ClientProvider>,
-}
 
 trait ClientProvider: Send + Sync {
     fn client(&self) -> &Client;
 }
 
+pub struct IrohStorage {
+    client_provider: Arc<dyn ClientProvider>,
+}
+
+impl Clone for IrohStorage {
+    fn clone(&self) -> Self {
+        IrohStorage {
+            client_provider: Arc::clone(&self.client_provider),
+        }
+    }
+}
+
+#[derive(Clone)]
 struct ClientHolder {
     client: Client,
 }
@@ -41,30 +51,38 @@ impl<S: iroh::blobs::store::Store> ClientProvider for NodeHolder<S> {
     }
 }
 
+impl<S: Clone> Clone for NodeHolder<S> {
+    fn clone(&self) -> Self {
+        NodeHolder {
+            node: self.node.clone(),
+        }
+    }
+}
+
 impl IrohStorage {
     pub async fn from_path(root: impl AsRef<Path>) -> Result<Self> {
         let client = Client::connect_path(root).await?;
         Ok(Self {
-            client_provider: Box::new(ClientHolder { client }),
+            client_provider: Arc::new(ClientHolder { client }),
         })
     }
 
     pub async fn from_addr(addr: SocketAddr) -> Result<Self> {
         let client = Client::connect_addr(addr).await?;
         Ok(Self {
-            client_provider: Box::new(ClientHolder { client }),
+            client_provider: Arc::new(ClientHolder { client }),
         })
     }
 
     pub fn from_client(client: Client) -> Self {
         Self {
-            client_provider: Box::new(ClientHolder { client }),
+            client_provider: Arc::new(ClientHolder { client }),
         }
     }
 
     pub fn from_node<S: iroh::blobs::store::Store + 'static>(node: iroh::node::Node<S>) -> Self {
         Self {
-            client_provider: Box::new(NodeHolder { node }),
+            client_provider: Arc::new(NodeHolder { node }),
         }
     }
 
@@ -83,6 +101,10 @@ impl IrohStorage {
     }
 }
 
+fn parse_hash(hash: &str) -> Result<Hash, StorageError> {
+    Hash::from_str(hash).map_err(|e| StorageError::InvalidHash(hash.to_string(), e.to_string()))
+}
+
 #[async_trait]
 impl Storage for IrohStorage {
     async fn upload_bytes(&self, bytes: impl Into<Bytes> + Send) -> Result<String> {
@@ -90,19 +112,24 @@ impl Storage for IrohStorage {
         Ok(blob.hash.to_string())
     }
 
-    async fn download_bytes(&self, hash: &str) -> Result<Bytes> {
-        let hash = iroh::blobs::Hash::from_str(hash)?;
+    async fn download_bytes(&self, hash: &str) -> Result<Bytes, StorageError> {
+        let hash = parse_hash(hash)?;
         let res = self.client().blobs().read_to_bytes(hash).await?;
         Ok(res)
     }
 
-    async fn iter_chunks(
-        &self,
-        hash: &str,
-    ) -> Result<Pin<Box<dyn Stream<Item = Result<Bytes>> + Send>>> {
-        let hash = Hash::from_str(hash)?;
-        let reader = self.client().blobs().read(hash).await?;
-        let total_size = reader.size();
+    async fn iter_chunks(&self, hash: &str) -> Result<ByteStream, StorageError> {
+        let hash = parse_hash(hash)?;
+        let reader = self.client().blobs().read(hash).await;
+        if let Err(e) = reader {
+            let err_str = e.to_string();
+            if err_str.contains("not found") {
+                return Err(StorageError::BlobNotFound(hash.to_string()));
+            } else {
+                return Err(StorageError::StorageError(e.into()));
+            }
+        }
+        let total_size = reader?.size();
 
         let stream = stream::unfold(
             (self.client().blobs().clone(), hash, 0u64, total_size),
--- storage/src/storage.rs
+++ storage/src/storage.rs
@@ -6,14 +6,28 @@ use async_trait::async_trait;
 use bytes::Bytes;
 use futures::Stream;
 use std::pin::Pin;
+use thiserror;
+
+#[derive(thiserror::Error, Debug)]
+pub enum Error {
+    #[error("Blob with hash {0} not found")]
+    BlobNotFound(String),
+
+    #[error("Storage error: {0}")]
+    StorageError(#[from] anyhow::Error),
+
+    #[error("Invalid hash {0}. Error: {1}")]
+    InvalidHash(String, String),
+
+    #[error("Error occurred: {0}")]
+    Other(#[source] anyhow::Error),
+}
+
+pub type ByteStream = Pin<Box<dyn Stream<Item = Result<Bytes>> + Send>>;
 
 #[async_trait]
-pub trait Storage: Send {
+pub trait Storage: Send + Clone {
     async fn upload_bytes(&self, bytes: impl Into<Bytes> + Send) -> Result<String>;
-    async fn download_bytes(&self, hash: &str) -> Result<Bytes>;
-
-    async fn iter_chunks(
-        &self,
-        hash: &str,
-    ) -> Result<Pin<Box<dyn Stream<Item = Result<Bytes>> + Send>>>;
+    async fn download_bytes(&self, hash: &str) -> Result<Bytes, Error>;
+    async fn iter_chunks(&self, hash: &str) -> Result<ByteStream, Error>;
 }
